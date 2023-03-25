b0f7e437e0d9017a6a6472ad656e5b040d1f8286
hp <sajid.faisal123@gmail.com>
2023-03-26 00:02:50 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+const newStyle={
+  color:'blue',
+  backgroundColor:'black',
+  padding:'20px',
+  borderRadius:'5px'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Myself></Myself>
+       <Myself></Myself>
+       <Myself></Myself>
     </div>
   );
+}
+
+function Myself(){
+  return (
+  <div className='heading'>
+    <h1 >I'm Faisal Tajwar</h1>
+    <p>Im Live in Ashulia</p>
+  </div>
+  )
 }
 
 export default App;
